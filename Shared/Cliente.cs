d63792d6d25c619ac50c91e820bc63a885d684bb
Matheusmelo3535 <matheusmelo3535@gmail.com>
2020-10-02 17:23:19 -0300

    Projeto Base

---
+++ Shared/Cliente.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace MisterMuscle.Shared {
+    
+    public class Cliente {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
